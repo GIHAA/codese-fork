26d392428be14e73dd96a56bc6847399592b8a34
Nipun Palliyaguru <99266866+NipunPalliyaguru@users.noreply.github.com>
2024-05-14 10:06:08 +0530

    Update db.js

--- cook-recipe-backend/db/db.js
+++ cook-recipe-backend/db/db.js
@@ -1,23 +1,10 @@
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
-
-
 dotenv.config();
 
-const dbUri = process.env.ATLAS_URI;
-
-if (!dbUri) {
-    console.error("Database URI is missing. Check your .env file.");
-    process.exit(1);  // Exit the process if the URI is not found
-}
-
-mongoose.connect(dbUri, {
+mongoose.connect(process.env.ATLAS_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
+}).then((result)=>{
+    console.log('Connected to DB');
 })
-.then(() => {
-    console.log('Connected to MongoDB');
-})
-.catch((err) => {
-    console.error('Error connecting to MongoDB:', err.message);
-});
